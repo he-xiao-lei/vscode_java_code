45b1bde44c5dbc5ccee83294914e7e44c24f4cbf
vblaws <h3239498031@gmail.com>
2025-01-19 13:15:12 +0800

    2025年 01月 19日 星期日 13:15:11 CST

--- javastudy/src/IO/ZipStream/ZipStreamDemo1.java
+++ javastudy/src/IO/ZipStream/ZipStreamDemo1.java
@@ -2,6 +2,7 @@ package IO.ZipStream;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -32,12 +33,20 @@ public class ZipStreamDemo1 {
             System.out.println(zipEntry);
                 if (zipEntry.isDirectory()){
                     //文件夹：需要在目的地dest处创建一个同样的文件夹
-                    File file = new File(dest,zipEntry.getName());
+                    File file = new File(dest,zipEntry.toString());
                     file.mkdirs();
                 }else {
                     //文件：需要读取到压缩包中的文件，并把他存放到目的地dest文件夹中（按照层级目录进行存放）
+                    FileOutputStream fos = new FileOutputStream(new File(dest,zipEntry.toString()));
+                    int b;
 
+                    while ((b=zip.read())!=-1){
+                        fos.write(b);
+                    }
+                    fos.close();
+                    zip.closeEntry();
                 }
+                FileOutputStream fos = new FileOutputStream(new File())
         }
     }
 }
