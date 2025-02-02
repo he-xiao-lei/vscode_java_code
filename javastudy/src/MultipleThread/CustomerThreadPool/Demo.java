9d4a19da7b57abf6eb7a826765007f11857b217f
vblaws <h3239498031@gmail.com>
2025-02-03 07:36:41 +0800

    2025年 02月 03日 星期一 07:36:41 CST

--- javastudy/src/MultipleThread/CustomerThreadPool/Demo.java
+++ javastudy/src/MultipleThread/CustomerThreadPool/Demo.java
@@ -15,19 +15,12 @@ public class Demo {
 //        参数五：任务队列：不能为空（null）。
 //        参数六：创建线程工厂：不能为空（null）。
 //        参数七：任务的拒绝策略：不能为空（null）。
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3,
-                6,
-                60,
-                TimeUnit.SECONDS,
-                new ArrayBlockingQueue<>(10),
-                Executors.defaultThreadFactory(),
-                new ThreadPoolExecutor.DiscardPolicy());
-//        threadPoolExecutor.submit(()->{
-//            for (int i = 0; i < 100; i++) {
-//            }
-//        })
-        int poolSize = threadPoolExecutor.getPoolSize();
-        System.out.println("poolSize = " + poolSize);
-
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3,//        参数一：核心线程数量：不能小于 0
+                6,// 最大线程数：不能小于等于 0，且最大数量需大于等于核心线程数量
+                60,//        参数三：空闲线程最大存活时间：不能小于
+                TimeUnit.SECONDS,//        参数四：时间单位：用TimeUnit指
+                new ArrayBlockingQueue<>(10),//        参数五：任务队列：不能为空（null
+                Executors.defaultThreadFactory(),//        参数六：创建线程工厂：不能为空（null）。
+                new ThreadPoolExecutor.DiscardPolicy());//        参数七：任务的拒绝策略：不能为空（null）
     }
 }
